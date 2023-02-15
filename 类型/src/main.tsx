1a5ac9955b42d86e10e409aaa1c116e85ce311de
chenchongjun <chencj@huitone.com>
2023-02-15 13:27:47 +0800

    init

--- 类型/src/main.tsx
+++ 类型/src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+/*
+ * @Description:
+ * @Author: 陈崇俊
+ * @Date: 2023-02-15 13:21:51
+ * @LastEditors: 陈崇俊
+ * @LastEditTime: 2023-02-15 13:26:21
+ */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
